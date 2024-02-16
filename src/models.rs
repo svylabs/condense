8ca07b982481f448d66efde0223706aa889f7d3d
svylabs <sg@svylabs.com>
2024-02-16 18:05:00 +0100

    Adding users and roles in the same transaction

--- src/models.rs
+++ src/models.rs
@@ -15,11 +15,30 @@ pub struct NewUser {
     pub username: String,
 }
 
-#[derive(Queryable, Selectable, Insertable)]
+#[derive(Queryable, Selectable, Insertable, Debug, Serialize, Deserialize)]
 #[diesel(table_name = crate::schema::roles)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct Role {
     pub id: i32,
     pub name: String,
     pub description: String,
+}
+
+#[derive(Insertable, Debug, Serialize, Deserialize)]
+#[diesel(table_name = crate::schema::user_roles)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct NewUserRole {
+    pub user_id: i32,
+    pub role_id: i32
+}
+
+impl NewUserRole {
+    pub fn new(user_id: i32, roles: &Vec<Role>) -> Vec<NewUserRole> {
+        roles.into_iter().map(|role| {
+            NewUserRole {
+                user_id,
+                role_id: role.id
+            }
+        }).collect::<Vec<NewUserRole>>()
+    }
 }
--- src/users/mod.rs
+++ src/users/mod.rs
@@ -4,24 +4,44 @@ use diesel::prelude::*;
 use diesel::{connection, Connection, PgConnection, RunQueryDsl};
 
 use crate::{state::app_state::AppState, users::types::NewUserInput};
-use crate::schema::users;
-use crate::models::{NewUser, User};
+use crate::schema::{users, roles};
+use crate::models::{NewUser, NewUserRole, Role, User};
 
 #[post("/new")]
 async fn new_user(body: web::Json<NewUserInput>, state: web::Data<AppState>) -> impl Responder {
     let mut pool = state.pool.get().unwrap();
-    let new_user = body.into_inner();
+    let new_user_input = body.into_inner();
     let new_user = NewUser {
-        username: new_user.username,
+        username: new_user_input.username,
     };
-    let inserted_user = diesel::insert_into(users::table)
-        .values(&new_user)
-        .get_result::<User>(&mut pool)
-        .unwrap();
-    HttpResponse::Ok().body(format!("New user created: {:?}", inserted_user))
+    let result = pool.transaction::<User, diesel::result::Error, _>(|pool| {
+        let inserted_user = diesel::insert_into(users::table)
+            .values(&new_user)
+            .get_result::<User>(pool)
+            .unwrap();
+        let all_roles = new_user_input.roles.unwrap();
+        let total_roles = all_roles.len();
+        let user_roles = roles::table
+            .filter(roles::name.eq_any(all_roles))
+            .load::<Role>(pool)
+            .expect("Error loading roles");
+        let user_roles_to_insert = NewUserRole::new(inserted_user.id, &user_roles);
+        diesel::insert_into(crate::schema::user_roles::table)
+            .values(&user_roles_to_insert)
+            .execute(pool)
+            .expect("Error inserting user roles");
+        match total_roles != user_roles.len() {
+            true =>  Err(diesel::result::Error::NotFound),
+            false => Ok(inserted_user)
+        }
+    });
+    match result {
+        Ok(inserted_user) => HttpResponse::Ok().body(format!("New user created: {:?}", inserted_user)),
+        Err(e) => HttpResponse::BadRequest().body(format!("Error creating user: {:?}", e))
+    }
 }
 
-#[get("/round2")]
+#[get("/list")]
 async fn round2() -> impl Responder {
     HttpResponse::Ok().body("Round 2")
 }
